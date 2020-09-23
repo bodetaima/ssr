4f1c3f286646ed8a7e85461180e17a0208b754da
ACareer <acareer@anttech.com.vn>
2020-09-23 16:20:20 +0700

    update

--- src/template.js
+++ src/template.js
@@ -1,13 +1,11 @@
 export default function template(title, initialState = {}) {
-  let scripts = ` <script>
-                    window.__STATE__ = ${JSON.stringify(initialState)}
-                  </script>
-                  <script src="assets/client.js"></script>
-                `;
   let page = `<!DOCTYPE html>
               <html lang="en">
               <head>
                 <meta charset="utf-8">
+                <script>
+                  window.__STATE__ = ${JSON.stringify(initialState)}
+                </script>
                 <title> ${title} </title>
                 <link rel="stylesheet" href="assets/style.css">
               </head>
@@ -17,7 +15,7 @@ export default function template(title, initialState = {}) {
                    </div>
                 </div>
 
-                  ${scripts}
+                <script src="assets/client.js"></script>
               </body>
               `;
 
